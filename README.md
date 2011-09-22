1a0b25069c4e2200ae5e4a87743875b219c468e8
Viktor Leonhardt <leonharv@unix-ag.uni-kl.de>
2011-09-22 09:12:33 +0000

    implement usage of selects

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
-A simple spinbox for jQuery
-=====================================
+# A simple spinbox/selectbox for jQuery
 
 This plugin is also optimized for smartphones.
 
-Installation
-------------
+## Installation
 
 Simply include the 'jquery.tapbox.js' file in the head of your website. You also need jQuery and make shure, that jQuery is included bevore the plugin.
 
@@ -13,16 +11,27 @@ Simply include the 'jquery.tapbox.js' file in the head of your website. You also
 	 <script src="jquery.tapbox.js"></script>
 	</head>
 
-Usage
------
+## Usage
 
-Assuming you have an element that you'd like to bind the spinbox to:
+Assuming you have an element that you'd like to bind the tapBox to:
 
 	<input type="text" name="max_items" id="spinbox" />
+	
+or
+
+	<select name="lang" id="selectbox">
+		<option value="EN">English</option>
+		<option value="DE">Germany</option>
+		<option value="RU">Russian</option>
+	</select>
     
 You can set this input as a spinbox simply like this:
 
 	$('#spinbox').tapbox();
+	
+or
+
+	$('#selectbox').tapBox();
 	
 Additional you can define some options:
 
@@ -34,12 +43,41 @@ Additional you can define some options:
 	
 	$('#spinbox').tapbox(options);
 	
-Options
--------
+## Options
+### General:
+
+* wraper:
+
+>		Defines the object of the buttons eg. `<span>` or `<div>`
+
+* customClass:
+
+>		Defines a custom class
+
+* upText:
+
+>		Defines the text of the up-button
+
+* downText:
+
+>		Defines the text of the down-button
+
+### Spinbox:
 
 * max:
-	Defines the maximal number.
+
+>		Defines the maximal number.
+
 * min:
-	Defines the minimal number.
+
+>		Defines the minimal number.
+
 * steps:
-	Defines the steps of increasing/decreasing of the counter
+
+>		Defines the steps of increasing/decreasing of the counter
+
+### Selectbox:
+
+* repeat:
+
+>		Enables to repeat the select-list
--- jquery.tapbox.js
+++ jquery.tapbox.js
@@ -1,55 +1,122 @@
 /*
- * tapBox 1.0 - jQuery spinbox plugin
+ * tapBox 1.1 - jQuery spinbox/selectbox Plugin
  *
  * Copyright (c) 2011 Viktor Leonhardt (leonharv@unix-ag.uni-kl.de)
  * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
  * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
  *
- * Usage:
- *
- * $('.foo').tapBox({
- * max: 100,
- * min: 0,
- * steps: 1
- * });
+ * Usage information: https://github.com/leonharv/jQuery-tapBox
  *
  */
 
 jQuery.fn.tapBox = function(options){
-
-	settings = {
-		max: 100,
-		min: -100,
-		'steps': 1
-	};
-	
-	$.extend(settings, options);
+	// set defaults
+	$(this).data('tapBox', {
+		settings : {
+			max: 100,
+			min: -100,
+			'steps': 1,
+			'wrapper': 'span',
+			'customClass': 'tap',
+			'upText': '+',
+			'downText': '-',
+			'repeat': true
+		}
+	});
+	var settings = $.extend($(this).data('tapBox').settings, options);
   
-  // create the down-button
-  var downButton = $('<div/>', {'class': 'tapDown', 'style': 'cursor:pointer;'}).text('-');
-  // create the up-button
-  var upButton = $('<div/>', {'class': 'tapUp', 'style': 'cursor:pointer;'}).text('+');
+  // create the template down-button
+  var downButton = $('<' + settings.wrapper + '/>', {'class': settings.customClass + 'Down', 'style': 'cursor:pointer;'}).text(settings.downText);
+  // create the template up-button
+  var upButton = $('<' + settings.wrapper + '/>', {'class': settings.customClass + 'Up', 'style': 'cursor:pointer;'}).text(settings.upText);
 			  
-  return this.each(function(index) {
-  	// save the current element
-  	tapBoxElement = $(this).addClass('tapDisplay');
-  	// insert the up/down-tapBox
-  	$(this).before(downButton);
-    $(this).after(upButton);
-    
-    // set event for downButton
-    $('.tapDown').live('click', function(){
-    	var number = parseFloat( $(this).next('.tapDisplay').val() ) - settings.steps;
-    	if(number >= settings.min)
-    		$(this).next('.tapDisplay').val( number );
-    });
-    
-    // set event for upButton
-    $('.tapUp').live('click', function(){
-    	var number = parseFloat( $(this).prev('.tapDisplay').val() ) + settings.steps;
-    	if(number <= settings.max)
-    		$(this).prev('.tapDisplay').val( number );
-    });
+  return this.each(function(index,Element) {
+  	switch(this.tagName){
+  		case 'SELECT':
+  			// hide the selectbox and identify it
+  			$(this).hide().addClass('tapBox');
+  			// create display
+  			var display = $('<span/>', {'class': settings.customClass + 'Display'});
+  			// insert the selected text in the display
+  			display.text($(this).find('option:selected').text());
+  			// copy the templateButtons
+  			var selDownButton = downButton;
+				var selUpButton = upButton;
+  			// insert display and both buttons in the DOM
+  			$(this).after(selDownButton, display, selUpButton);
+  			
+  			// set event for downButton
+				$(selDownButton).click(function(){
+					if(!$(Element).find('option:selected').is('option:first-child')) {
+						// save the selected entry
+						var current = $(Element).find('option:selected');
+						// deselect all
+						$(Element).find('option').removeAttr('selected');
+						// select the previous
+						$(current).prev().attr('selected', true);
+						// update the display
+						$(this).next('.tapDisplay').text($(current).prev().text());
+					} else {
+						if(settings.repeat) {
+							// deselect all
+							$(Element).find('option').removeAttr('selected');
+							// select the first entry
+							$(Element).find('option:last').attr('selected', true);
+							// update the display
+							$(this).next('.tapDisplay').text($(Element).find('option:last').text());
+						}
+					}
+				});
+				
+				// set event for upButton
+				$(selUpButton).click(function(){
+					if(!$(Element).find('option:selected').is('option:last-child')) {
+						// save the selected entry
+						var current = $(Element).find('option:selected');
+						// deselect all
+						$(Element).find('option').removeAttr('selected');
+						// select the next
+						$(current).next().attr('selected', true);
+						// update the display
+						$(this).prev('.tapDisplay').text($(current).next().text());
+					} else {
+						if(settings.repeat) {
+							// deselect all
+							$(Element).find('option').removeAttr('selected');
+							// select the last entry
+							$(Element).find('option:first').attr('selected', true);
+							// update the display
+							$(this).prev('.tapDisplay').text($(Element).find('option:first').text());
+						}
+					}
+				});
+  			break;
+  		case 'INPUT':
+				// save the current element
+				$(this).addClass(settings.customClass + 'Display');
+				var intDownButton = downButton;
+				var intUpButton = upButton;
+				// insert the up/down-tapBox
+				$(this).before(intDownButton);
+				$(this).after(intUpButton);
+				
+				// set event for downButton
+				$(intDownButton).click(function(){
+					var number = parseFloat( $(this).next('.' + settings.customClass + 'Display').val() ) - settings.steps;
+					if(number >= settings.min)
+						$(this).next('.' + settings.customClass + 'Display').val( number );
+				});
+				
+				// set event for upButton
+				$(intUpButton).click(function(){
+					var number = parseFloat( $(this).prev('.' + settings.customClass + 'Display').val() ) + settings.steps;
+					if(number <= settings.max)
+						$(this).prev('.' + settings.customClass + 'Display').val( number );
+				});
+				break;
+			default:
+				alert('Please use TapBox only for Selects and Intputs, you are trying to use \'' + Element.tagName + '\'');
+		}
   });
 }
 
